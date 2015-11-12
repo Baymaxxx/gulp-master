3bb23057f24242aa5c31ca52602cfd6b47ae2ea8
Baymaxxx <81243232@qq.com>
2015-11-12 14:40:08 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 /**
  * 初始化
- * npm install gulp-util gulp-livereload gulp-imagemin imagemin-pngquant gulp-ruby-sass gulp-sourcemaps gulp-changed gulp-webserver gulp-minify-css gulp-uglify gulp-rename gulp-concat gulp-clean tiny-lr --save-dev
+ * npm install gulp-util gulp-autoprefixer gulp-compass gulp-livereload gulp-imagemin imagemin-pngquant gulp-ruby-sass gulp-sourcemaps gulp-changed gulp-webserver gulp-minify-css gulp-uglify gulp-rename gulp-concat gulp-clean tiny-lr --save-dev
  */
 // 引入gulp
 var gulp			= require('gulp');					// 基础库
 
 // 引入我们的gulp组件
-var sass 			=  require('gulp-ruby-sass'),			// CSS预处理/Sass编译
-	uglify 			=  require('gulp-uglify'),				// JS文件压缩
+var sass 			=  require('gulp-ruby-sass'),		// CSS预处理/Sass编译
+	uglify 			=  require('gulp-uglify'),			// JS文件压缩
 	imagemin 		=  require('gulp-imagemin'),		// imagemin 图片压缩
 	pngquant 		=  require('imagemin-pngquant'),	// imagemin 深度压缩
-	livereload 		=  require('gulp-livereload'),			// 网页自动刷新（服务器控制客户端同步刷新）
+	livereload 		=  require('gulp-livereload'),		// 网页自动刷新（服务器控制客户端同步刷新）
 	webserver 		= require('gulp-webserver'),		// 本地服务器
 	rename 			= require('gulp-rename'),			// 文件重命名
 	sourcemaps 		= require('gulp-sourcemaps'),		// 来源地图
 	changed 		= require('gulp-changed'),			// 只操作有过修改的文件
 	concat 			= require("gulp-concat"), 			// 文件合并
-	clean 			= require('gulp-clean');				// 文件清理
-
+	clean 			= require('gulp-clean'),			// 文件清理
+	autoprefixer = require('gulp-autoprefixer');		// css后处理，加css3前缀
 /* = 全局设置
 -------------------------------------------------------------- */
 var srcPath = {
@@ -43,18 +43,20 @@ var destPath = {
 	});
 	// 样式处理 压缩成compressed
 	gulp.task('cssmin', function () {
-     return sass(srcPath.css+'/*.scss', { style: 'compressed' }) // 指明源文件路径、并进行文件匹配（style: 'compressed' 表示输出格式）
+ 	    return sass(srcPath.css+'/*.scss', { style: 'compressed' }) // 指明源文件路径、并进行文件匹配（style: 'compressed' 表示输出格式）
           .on('error', function (err) {
                console.error('Error!', err.message); // 显示错误信息
           })
+          .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
           .pipe(gulp.dest(destPath.css)); // 输出路径
 	});
 	//样式处理 不压缩
 	gulp.task('sass', function () {
-     return sass(srcPath.css+'/*.scss', { style: 'compact' }) // 指明源文件路径、并进行文件匹配（style: 'compressed' 表示输出格式）
+   	  return sass(srcPath.css+'/*.scss', { style: 'nested' }) // 指明源文件路径、并进行文件匹配（style: 'compressed' 表示输出格式）
           .on('error', function (err) {
                console.error('Error!', err.message); // 显示错误信息
           })
+          .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
           .pipe(gulp.dest(srcPath.css)); // 输出路径
 	});
 	// JS文件压缩&重命名
