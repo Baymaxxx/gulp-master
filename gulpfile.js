de1aaf565ea0383f2be076a9e351f21a94ab3d27
Baymaxxx <Baymaxxx@users.noreply.github.com>
2015-11-13 10:53:04 +0800

    完善 gulpfile.js
    	gulp.task('help',function () {
    		console.log('----------------- 开发环境 -----------------');
    		console.log('gulp default		开发环境（默认任务）');
    		console.log('gulp html		HTML处理');
    		console.log('gulp sass		样式处理');
    		console.log('gulp script		JS文件压缩&重命名');
    		console.log('gulp images		图片压缩');
    		console.log('gulp concat		文件合并');
    		console.log('---------------- 发布环境 -----------------');
    		console.log('gulp release		打包发布');
    		console.log('gulp clean		清理文件');
    		console.log('gulp sassRelease		样式处理');
    		console.log('gulp scriptRelease	脚本压缩&重命名');
    		console.log('---------------------------------------------');
    	});

--- gulpfile.js
+++ gulpfile.js
@@ -18,19 +18,19 @@ var sass 			=  require('gulp-ruby-sass'),		// CSS预处理/Sass编译
 	concat 			= require("gulp-concat"), 			// 文件合并
 	clean 			= require('gulp-clean'),			// 文件清理
 	autoprefixer = require('gulp-autoprefixer');		// css后处理，加css3前缀
-/* = 全局设置
+/* = 全局路径设置
 -------------------------------------------------------------- */
 var srcPath = {
 	html	: 'src',
 	css		: 'src/css',
 	script	: 'src/js',
-	image	: 'src/images'
+	image	: 'src/img'
 };
 var destPath = {
 	html	: 'dist',
 	css     : 'dist/css',
 	script	: 'dist/js',
-	image	: 'dist/images'
+	image	: 'dist/img'
 };
 
 /* = 开发环境( Ddevelop Task )
@@ -41,9 +41,15 @@ var destPath = {
 			.pipe(changed( destPath.html ))
 			.pipe(gulp.dest( destPath.html ));
 	});
+	// .css处理
+	gulp.task('css', function() {
+		return gulp.src( srcPath.css+'/**/*.css' )
+			.pipe(changed( destPath.css ))
+			.pipe(gulp.dest( destPath.css ));
+	});
 	// 样式处理 压缩成compressed
 	gulp.task('cssmin', function () {
- 	    return sass(srcPath.css+'/*.scss', { style: 'compressed' }) // 指明源文件路径、并进行文件匹配（style: 'compressed' 表示输出格式）
+ 	    return sass(srcPath.css+'/**/*.scss', { style: 'compressed' }) // 指明源文件路径、并进行文件匹配（style: 'compressed' 表示输出格式）
           .on('error', function (err) {
                console.error('Error!', err.message); // 显示错误信息
           })
@@ -52,7 +58,7 @@ var destPath = {
 	});
 	//样式处理 不压缩
 	gulp.task('sass', function () {
-   	  return sass(srcPath.css+'/*.scss', { style: 'nested' }) // 指明源文件路径、并进行文件匹配（style: 'compressed' 表示输出格式）
+   	  return sass(srcPath.css+'/**/*.scss', { style: 'nested' }) // 指明源文件路径、并进行文件匹配（style: 'compressed' 表示输出格式）
           .on('error', function (err) {
                console.error('Error!', err.message); // 显示错误信息
           })
@@ -61,10 +67,10 @@ var destPath = {
 	});
 	// JS文件压缩&重命名
 	gulp.task('script', function() {
-		return gulp.src( [srcPath.script+'/*.js','!'+srcPath.script+'/*.min.js'] ) // 指明源文件路径、并进行文件匹配，排除 .min.js 后缀的文件
+		return gulp.src( [srcPath.script+'/**/*.js','!'+srcPath.script+'/**/*.min.js'] ) // 指明源文件路径、并进行文件匹配，排除 .min.js 后缀的文件
 			.pipe(changed( destPath.script )) // 对应匹配的文件
 			.pipe(sourcemaps.init()) // 执行sourcemaps
-			.pipe(rename({ suffix: '.min' })) // 重命名
+			// .pipe(rename({ suffix: '.min' })) // 重命名
 			.pipe(uglify({ preserveComments:'some' })) // 使用uglify进行压缩，并保留部分注释
 			.pipe(sourcemaps.write('maps')) // 地图输出路径（存放位置）
 			.pipe(gulp.dest( destPath.script )); // 输出路径
@@ -82,7 +88,7 @@ var destPath = {
 	});
 	// 文件合并
 	gulp.task('concat', function () {
-		return gulp.src( srcPath.script+'/*.min.js' )  // 要合并的文件
+		return gulp.src( srcPath.script+'/**/*.min.js' )  // 要合并的文件
 		.pipe(concat('libs.js')) // 合并成libs.js
 		.pipe(rename({ suffix: '.min' })) // 重命名
 		.pipe(gulp.dest( destPath.script )); // 输出路径
@@ -100,11 +106,11 @@ var destPath = {
 		// 监听 html
 		gulp.watch( srcPath.html+'/**/*.html' , ['html'])
 		// 监听 scss
-		gulp.watch( srcPath.css+'/*.scss' , ['sass','cssmin']);
+		gulp.watch( srcPath.css+'/**/*' , ['css','sass','cssmin']);
 		// 监听 images
 		gulp.watch( srcPath.image+'/**/*' , ['images']);
 		// 监听 js
-		gulp.watch( [srcPath.script+'/*.js','!'+srcPath.script+'/*.min.js'] , ['script']);
+		gulp.watch( [srcPath.script+'/*.js','!'+srcPath.script+'/*.min.js'] , ['script','concat']);
 	});
 	// 默认任务
 	gulp.task('default',['webserver','watch']);
